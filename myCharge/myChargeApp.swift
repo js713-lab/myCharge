924634469819c2ff1077e6bf0fc6e877f373ef9d
Yip Hong Seng <Js07ink@Gmail.com>
2024-05-31 10:57:37 +0800

    Initial Commit

---
+++ myCharge/myChargeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myChargeApp.swift
+//  myCharge
+//
+//  Created by Yip Hong Seng on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct myChargeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
